12a74d0807b4eeccebfa8b8334312570a879e657
bborelli <borelli.bruna@gmail.com>
2025-04-29 16:14:00 -0300

    Cleaned the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Weather from "./Weather";
-
 import "./App.css";
 
 export default function App() {
@@ -9,7 +8,7 @@ export default function App() {
       <h1>Weather App</h1>
       <Weather />
       <p>
-        This website was created by Bruna Borelli and can be seen on{" "}
+        This website was created by Bruna Borelli and can be seen on
         <a
           href="https://github.com/bborelli/weather-react"
           target="_blank"
