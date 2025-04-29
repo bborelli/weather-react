476e7a1fe97faacbed0d2ff9f1f1aaf06496ee26
bborelli <borelli.bruna@gmail.com>
2025-04-29 17:26:08 -0300

    Added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <p>
-        This website was created by Bruna Borelli and can be seen on
-        <a
-          href="https://github.com/bborelli/weather-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </p>
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was coded by Bruna Borelli and can be seen on
+          <a
+            href="https://github.com/bborelli/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
