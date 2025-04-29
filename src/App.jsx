970d33838c215c9e3408d12bf4e39bc5ee6b4726
bborelli <borelli.bruna@gmail.com>
2025-04-29 14:56:19 -0300

    Inserted Github link

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import Weather from "./Weather";
 
 import "./App.css";
 
-function App() {
-  return <Weather />;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather />
+      <p>
+        This website was created by Bruna Borelli and can be seen on{" "}
+        <a
+          href="https://github.com/bborelli/weather-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
-
-export default App;
